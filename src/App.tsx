249d9302cec1828125f8af755e99bddea9e2a776
Dang Tuan Ninh <dangninh020@gmail.com>
2025-02-05 14:35:55 +0700

    video 26

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "./redux/store";
 import { decrease, increment } from "./redux/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
 
 function App() {
-  const dispatch = useDispatch();
-  const count = useSelector((state: RootState) => state.counter);
+  const dispatch = useAppDispatch();
+
+  // const count = useSelector((state: RootState) => state.counter);
+  const count = useAppSelector((state) => state.counter);
+
   console.log(">>Check count:", count);
+
   return (
     <>
       <div>
